086e03b1de626e2f9d693fe3a49d65839dd87f2f
kriparam <53045082+kriparam@users.noreply.github.com>
2019-12-20 13:59:41 +0530

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author kpsingh
+ */
+public class Helloworld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("jhjgj");
+        int a=152,b=575,c;
+        c=a+b;
+        System.out.println(c);
+    }
+    
+}
